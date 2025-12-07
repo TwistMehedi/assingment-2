2d0a7863632f1386ff2a78ec824e2056d27d021a
TwistMehedi <ataul1708@gmail.com>
2025-12-08 00:25:08 +0600

    cddd

--- src/server.ts
+++ src/server.ts
@@ -13,6 +13,7 @@ app.use(express.json());
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/vehicles", vehicleRouter);
+
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/bookings", bookingRouter);
  
